93136f73ae5757bfbc283d2c23b47d1333d086ec
Luis Hernandez <luis.jose.hernandez@hotmail.com>
2024-01-06 11:45:55 -0400

    modulo 2

--- web/models.py
+++ web/models.py
@@ -12,7 +12,7 @@ class Categoria(models.Model):
 class Producto(models.Model): # ForeinKey porque es una relacion de 1 a muchos, es decir un celular muchos modelos de celelulares
     categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT) #RESTRICT es cuando alguien borre una categoria no le permita si ya tiene productos creados, para evitar que haya incrongruencia de datos.
     nombre = models.CharField(max_length=200)
-    descripcion = models.TextField(null=True) # para que acepte mas de 250 caracteres y nuññ por si no quiere colocar nada. 
+    descripcion = models.TextField(null=True) # para que acepte mas de 250 caracteres y null por si no quiere colocar nada. 
     precio = models.DecimalField(max_digits=9, decimal_places=2)
     fecha_registro = models.DateTimeField(auto_now_add=True)
     imagen = models.ImageField(upload_to='productos', blank=True)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Categoria, Producto
 
 # Create your views here.
+""" VISTAS PARA EL CATALOGO DE PRODUCTOS """
+def index(request):
+    listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
+
+    context = {
+        'productos':listaProductos,
+        'categorias':listaCategorias
+    }
+    return render(request, 'index.html', context)
+
+""" vista para filtrar productos por categoria """
+def productosPorCategoria(request,categoria_id):
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+    
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+
+""" vista para filtrado de productos por nombre """
+def productosPorNombre(request):
+    nombre = request.POST['nombre']
+    
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+
+""" vista para el detalle de producto"""
+def productoDetalle(request,producto_id):
+    #objProducto = Producto.objects.get(pk=producto_id)
+    objProducto = get_object_or_404(Producto,pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    
+    return render(request,'producto.html',context)
+
+
+
